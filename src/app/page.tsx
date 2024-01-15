d7fb6c243ca2ca25d81034c2df5d856481b216cb
Cleyson <cleysonph@gmail.com>
2024-01-14 22:38:30 -0300

    change home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default async function Home() {
     <main className="main">
       <div>
         <h2>Hello World</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor temporibus ipsam quidem illo nemo, aut, est adipisci doloribus, eligendi rem alias! Ullam voluptatibus quibusdam minus facilis sunt nesciunt, deleniti quas!</p>
       </div>
       <Link href="/about">About</Link>
       <br />
